1b1073544570d4a4280942e321ce30a5b41e9082
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-03-15 08:19:08 -0400

    Make lexical editor react to change to respective graph node (#13)

--- src/app/editor/Editor.tsx
+++ src/app/editor/Editor.tsx
@@ -1,23 +1,21 @@
-import {
-  $createParagraphNode,
-  $createTextNode,
-  $getRoot,
-  EditorState,
-} from "lexical";
+import { $createParagraphNode, $createTextNode, $getRoot, $setSelection, EditorState } from "lexical";
 
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { KeyboardOverridesPlugin } from "./KeyboardOverridesPlugin";
 import { ContentEditable } from "./ui/ContentEditable";
 
-import styles from "./Editor.module.css";
-import { useViewStore } from "../store/outline";
-import { OnChangePlugin } from "./OnChangePlugin";
-import { MentionPlugin } from "./MentionPlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { observer } from "mobx-react-lite";
+import { useCallback, useEffect } from "react";
 import { GraphNodeView } from "../model/GraphNodeView";
+import { useViewStore } from "../store/outline";
+import styles from "./Editor.module.css";
+import { MentionPlugin } from "./MentionPlugin";
+import { OnChangePlugin } from "./OnChangePlugin";
 
 const theme = {
   // Theme styling goes here
@@ -42,7 +40,7 @@ interface Props {
   context: EditorContext;
 }
 
-export const Editor = ({ node, onChange, context }: Props) => {
+export const Editor = ({ node, context }: Props) => {
   const outlineViewStore = useViewStore();
   const initialConfig = {
     namespace: "MyEditor",
@@ -57,18 +55,8 @@ export const Editor = ({ node, onChange, context }: Props) => {
     },
   };
 
-  const editorOnChange = (editorState: EditorState) => {
-    editorState.read(() => {
-      const text = $getRoot().getTextContent();
-      onChange(text);
-    });
-  };
-
   return (
-    <div
-      className={styles.EditorWrapper}
-      onFocus={() => outlineViewStore.setFocusedNode(node)}
-    >
+    <div className={styles.EditorWrapper} onFocus={() => outlineViewStore.setFocusedNode(node)}>
       <LexicalComposer initialConfig={initialConfig}>
         <PlainTextPlugin
           ErrorBoundary={LexicalErrorBoundary}
@@ -78,10 +66,68 @@ export const Editor = ({ node, onChange, context }: Props) => {
         />
         {node.isFocused && <AutoFocusPlugin />}
         <HistoryPlugin />
-        <OnChangePlugin onChange={editorOnChange} />
+        <SyncEditorAndGraphNode node={node} />
         <KeyboardOverridesPlugin nodeView={node} context={context} />
         <MentionPlugin />
       </LexicalComposer>
     </div>
   );
 };
+
+/**
+ * This component is responsible for keeping the Lexical editor state in sync
+ * with the graph node state. It listens for changes in the editor state and
+ * updates the graph node accordingly. It also listens for changes in the graph
+ * node state and updates the editor accordingly. It only applies a change if
+ * the new state is different from the current state, to avoid infinite loops.
+ *
+ * (TODO: This way of avoiding infinite loops feels a bit sketchy, but it works
+ * for now)
+ */
+const SyncEditorAndGraphNode = observer(({ node }: { node: GraphNodeView }) => {
+  const [editor] = useLexicalComposerContext();
+
+  const setGraphNodeTextToEditorState = useCallback(
+    (editorState: EditorState) => {
+      editorState.read(() => {
+        const text = $getRoot().getTextContent();
+        if (text === node.graphNode.text) return;
+        node.graphNode.setText(text);
+      });
+    },
+    [node],
+  );
+
+  const setEditorToGraphNodeText = useCallback(
+    (text: string) => {
+      editor.update(() => {
+        const currentParagraph = $getRoot().getChildren()[0];
+        if (currentParagraph.getTextContent() === text) return;
+        const paragraph = $createParagraphNode();
+        paragraph.append($createTextNode(text));
+        currentParagraph.replace(paragraph);
+        /**
+         * Setting the selection to null here seems to prevent the error below.
+         * Based on https://stackoverflow.com/a/72197580, it seems that when we're
+         * updating the editor state on a non-focused editor, a new selection is
+         * automatically set in the new editor state, and then the editor takes
+         * the dom selection away from the user, leading to other downstream issues.
+         *
+         * ```
+         * Error: updateEditor: selection has been lost because the previously
+         * selected nodes have been removed and selection wasn't moved to
+         * another node. Ensure selection changes after removing/replacing a
+         * selected node.
+         * ```
+         */
+        $setSelection(null);
+      });
+    },
+    [editor],
+  );
+
+  useEffect(() => {
+    setEditorToGraphNodeText(node.graphNode.text);
+  }, [setEditorToGraphNodeText, node.graphNode.text]);
+  return <OnChangePlugin onChange={setGraphNodeTextToEditorState} />;
+});
