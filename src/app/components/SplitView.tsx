5fcbf82e676f497b47f322be96f771dc15ceeafd
Agu <agugalgui@gmail.com>
2024-05-09 18:35:55 +0200

    fix: balance split view on small desktop screens + split hidden on mobile
    A bit of css refactor, better and balanced padding on split view for screens like macbooks (1440) and split button hidden on mobile.

--- src/app/components/SplitView.tsx
+++ src/app/components/SplitView.tsx
@@ -1,15 +1,16 @@
 import { observer } from "mobx-react-lite";
 import { OutlineView } from "./OutlineView";
+import s from "./Splitview.module.css";
 import { ThoughtstreamView } from "./ThoughtstreamView";
 
 export const SplitView = observer(({ searchQuery }: { searchQuery: string }) => {
   return (
-    <div className="flex w-full max-w-7xl mx-auto">
-      <div className="flex w-full gap-8 justify-between">
-        <div className="w-1/2">
+    <div className={`${s.container} ${s.containerLarge}`}>
+      <div className={s.mainContent}>
+        <div className={s.halfWidth}>
           <ThoughtstreamView searchQuery={searchQuery} />
         </div>
-        <div className="w-1/2">
+        <div className={s.halfWidth}>
           <OutlineView searchQuery={searchQuery} />
         </div>
       </div>
