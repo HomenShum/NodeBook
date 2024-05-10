87a28016c77f2525001086a9921e1076168024f5
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-05-10 11:51:16 -0400

    fix: On colon keypress, set the graph object's content to the editor content following the selection position (#113)
    * wip
    
    * On colon keypress, set the graph object's content to the editor content following the selection position
    
    * Handle reverse selections
    
    * appease typescript

--- src/app/editor/plugins/RelationPlugin.tsx
+++ src/app/editor/plugins/RelationPlugin.tsx
@@ -1,33 +1,11 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { $getSelection, COMMAND_PRIORITY_NORMAL, KEY_DOWN_COMMAND, LexicalEditor } from "lexical";
+import { COMMAND_PRIORITY_NORMAL, KEY_DOWN_COMMAND } from "lexical";
 import { useEffect } from "react";
 import { useRelationAtPath } from "../../components/RelatedObject/RelatedObjectContext";
 import { useViewController } from "../../controller/useViewController";
 import { GraphNode } from "../../model/GraphNode";
 import { useGraphStore } from "../../store/useGraphStore";
-
-/**
- * Returns the text from the start of the editor to the left side of the selection.
- * If there's no selection, returns null.
- */
-function getTextBeforeSelection(editor: LexicalEditor): string | null {
-  return editor.getEditorState().read(() => {
-    // Get nodes up to and including the selection start node
-    const selection = $getSelection();
-    if (!selection) return null;
-    const points = selection.getStartEndPoints();
-    if (!points) return null;
-    const selectionStartNode = selection.getNodes()[0];
-    if (!selectionStartNode) return null;
-    const nodesBeforeSelection = selectionStartNode.getPreviousSiblings();
-    // Convert to text
-    const offset = selection.isBackward() ? points[1].offset : points[0].offset;
-    return (
-      nodesBeforeSelection.map((node) => node.getTextContent()).join("") +
-      selectionStartNode.getTextContent().slice(0, offset)
-    );
-  });
-}
+import { getChipsBetween, getSelectionPositions, getTextBetween } from "../utils";
 
 export const RelationPlugin = () => {
   const graphStore = useGraphStore();
@@ -44,31 +22,22 @@ export const RelationPlugin = () => {
         if (event.key !== ":") {
           return false;
         }
-
-        // After use presses :, we trigger logic to update the relation type to whatever the user was typing.
-        // There are exceptions, though. We don't do anything if:
-        // 1. The existing relation is already some special (non-child) relationship
-        // 2. The text in the editor contains non-plain text (e.g. mentions, etc.)
-
         // Only trigger logic when current node is a regular child of the rendered parent
         if (relation.relationType.id != graphStore.relationTypesById.child.id || relation.to.id != object.id) {
           return false;
         }
-
-        let textBeforeSelection = getTextBeforeSelection(editor);
-        if (textBeforeSelection === null) {
-          // If editorText is null here, indicates that the editor contains non-plain text, so we bail
-          return false;
-        }
-        textBeforeSelection = textBeforeSelection.trim();
-
-        let relationType = graphStore.getOrCreateRelationTypeByLabel(textBeforeSelection);
-        relation.setType(relationType);
-        object.setContent("");
-        viewController.setFocusedNode(pathToNodeStr);
-
         event.preventDefault();
         event.stopPropagation();
+        const [selectionLeft, selectionRight] = getSelectionPositions(editor).sort((a, b) =>
+          a.index === b.index ? a.offset - b.offset : a.index - b.index,
+        );
+        // Set the relation type to the text before the cursor
+        const textBefore = getTextBetween(editor, { index: 0, offset: 0 }, selectionLeft).trim();
+        let relationType = graphStore.getOrCreateRelationTypeByLabel(textBefore);
+        relation.setType(relationType);
+        // Set the content to the content after the cursor and focus
+        object.setContent(getChipsBetween(editor, selectionRight));
+        viewController.setFocusedNode(pathToNodeStr);
         return true;
       },
       COMMAND_PRIORITY_NORMAL,
--- src/app/editor/utils.ts
+++ src/app/editor/utils.ts
@@ -1,4 +1,4 @@
-import { $isTextNode, LexicalNode } from "lexical";
+import { $getRoot, $getSelection, $isParagraphNode, $isTextNode, LexicalEditor, LexicalNode } from "lexical";
 import { Chip } from "../model/GraphNode";
 import { $isMentionNode } from "../model/MentionNode";
 
@@ -10,4 +10,156 @@ export function nodeToChip(node: LexicalNode): Chip {
   } else {
     throw new Error("Unsupported node type");
   }
+}
+
+type LexicalEditorPosition = { index: number; offset: number };
+
+/**
+ * Returns the selection as a pair of positions in the editor.
+ * The first position is the selection anchor and the second is the focus.
+ */
+export function getSelectionPositions(editor: LexicalEditor): [LexicalEditorPosition, LexicalEditorPosition] {
+  return editor.getEditorState().read(() => {
+    const selection = $getSelection();
+    if (!selection) {
+      throw new Error("No selection found");
+    }
+    const points = selection.getStartEndPoints();
+    if (!points) {
+      throw new Error("Selection points not found");
+    }
+    const nodes: LexicalNode[] = selection.getNodes()[0].getParent()?.getChildren() || [];
+    const selectionStartIndex = nodes.findIndex((n) => n.getKey() === points[0].key);
+    const selectionEndIndex = nodes.findIndex((n) => n.getKey() === points[1].key);
+    if (selectionStartIndex === -1 || selectionEndIndex === -1) {
+      throw new Error("Selection points don't match any nodes");
+    }
+    return [
+      { index: selectionStartIndex, offset: points[0].offset },
+      { index: selectionEndIndex, offset: points[1].offset },
+    ];
+  });
+}
+
+/**
+ * The content of graph nodes is a flat list of text and mention nodes.
+ * This functions returns a flat list of lexical nodes which corresponds
+ * to that content.
+ */
+function getNodes(editor: LexicalEditor): LexicalNode[] {
+  return editor.getEditorState().read(() => {
+    const children = $getRoot().getChildren();
+    if (children.length === 0) {
+      return [];
+    }
+    if (children.length > 1) {
+      throw new Error("Expected only one child node");
+    }
+    const child = children[0];
+    if (!$isParagraphNode(child)) {
+      throw new Error("Expected a paragraph node");
+    }
+    return child.getChildren();
+  });
+}
+
+/**
+ * Returns the text content between two positions in the editor.
+ */
+export function getTextBetween(
+  editor: LexicalEditor,
+  from?: LexicalEditorPosition,
+  to?: LexicalEditorPosition,
+): string {
+  const nodes: LexicalNode[] = getNodes(editor);
+  if (!from) {
+    return nodes.map((node) => node.getTextContent()).join("");
+  }
+  const toDefined = to || { index: nodes.length - 1, offset: nodes[nodes.length - 1].getTextContent().length };
+  return nodes
+    .map((node, i) => {
+      if (i < from.index || i > toDefined!.index) {
+        return "";
+      } else if (i === from.index && i === toDefined.index) {
+        return node.getTextContent().slice(from.offset, toDefined.offset);
+      } else if (i === from.index) {
+        return node.getTextContent().slice(from.offset);
+      } else if (i === toDefined.index) {
+        return node.getTextContent().slice(0, toDefined.offset);
+      } else {
+        return node.getTextContent();
+      }
+    })
+    .join("");
+}
+
+/**
+ * Returns the content between two positions in the editor as a list of chips.
+ */
+export function getChipsBetween(
+  editor: LexicalEditor,
+  from?: LexicalEditorPosition,
+  to?: LexicalEditorPosition,
+): Chip[] {
+  const nodes = getNodes(editor);
+  if (!from) {
+    return nodes.map(nodeToChip);
+  }
+  const toDefined = to || { index: nodes.length - 1, offset: nodes[nodes.length - 1].getTextContent().length };
+  const chips: Chip[] = [];
+  nodes.forEach((node, i) => {
+    if (i < from.index || i > toDefined.index) {
+      // skip nodes outside of the range
+      return;
+    }
+    if (i === from.index && i === toDefined.index) {
+      chips.push({ type: "text", value: node.getTextContent().slice(from.offset, toDefined.offset) });
+    } else if (i === from.index) {
+      // handle start
+      if ($isMentionNode(node)) {
+        const text = node.getTextContent();
+        if (from.offset === 0) {
+          chips.push(nodeToChip(node));
+        } else if (from.offset < text.length) {
+          chips.push({ type: "text", value: text.slice(from.offset) });
+        } else {
+          // skip the mention node
+        }
+      } else if ($isTextNode(node)) {
+        const text = node.getTextContent();
+        if (from.offset < text.length) {
+          chips.push({ type: "text", value: text.slice(from.offset) });
+        } else {
+          // skip the text node
+        }
+      } else {
+        console.error("Unexpected node type", node);
+      }
+    } else if (i === toDefined.index) {
+      // handle end
+      if ($isMentionNode(node)) {
+        const text = node.getTextContent();
+        if (toDefined.offset >= text.length) {
+          chips.push(nodeToChip(node));
+        } else if (toDefined.offset > 0) {
+          chips.push({ type: "text", value: text.slice(0, toDefined.offset) });
+        } else {
+          console.error("Unexpected offset", toDefined);
+        }
+      } else if ($isTextNode(node)) {
+        const text = node.getTextContent();
+        if (toDefined.offset > 0) {
+          chips.push({ type: "text", value: text.slice(0, toDefined.offset) });
+        } else {
+          // skip the text node
+        }
+      } else {
+        console.error("Unexpected node type", node);
+      }
+    } else {
+      // handle middle
+      chips.push(nodeToChip(node));
+    }
+  });
+  return chips;
 }
