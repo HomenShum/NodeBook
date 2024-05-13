bf2e79cf568e632ce07c26cafe6597a871d39f95
Agu <agugalgui@gmail.com>
2024-05-13 19:38:07 +0200

    [ENT-3406] New design for pinned section
    Based on Jacob's feedback.

--- src/app/model/GraphStore.ts
+++ src/app/model/GraphStore.ts
@@ -69,7 +69,7 @@ export class GraphStore {
   constructor() {
     Object.values(defaultRelationTypes).forEach((rt) => this.createRelationType(rt, true));
     makeAutoObservable(this);
-    this.outlineRoot = this.createNode({ id: OUTLINE_ROOT_ID, content: [{ type: "text", value: "My Lists" }] });
+    this.outlineRoot = this.createNode({ id: OUTLINE_ROOT_ID, content: [{ type: "text", value: "My Graphs" }] });
     this.userRoot = this.createNode({ id: USER_ROOT_ID, content: [{ type: "text", value: "User" }] });
     this.thoughtstreamRoot = this.createNode({
       id: THOUGHTSTREAM_ROOT_ID,
