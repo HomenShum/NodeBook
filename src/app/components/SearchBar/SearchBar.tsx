4c48ce4d4b0d04a293b07ab0726b8bdb041ad0fd
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-05-15 09:08:49 -0400

    fix: Make x button in search bar clear it
    The x button was missing a setInput("") so wasn't clearing it.
    
    But we can just get rid of the extra input state, and use the search query
    instead. There was a comment about the component not updating in response
    but that was just because the search bar was missing an observer wrapper
    function.

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,25 +1,14 @@
 import { Search, X } from "lucide-react";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 import { useViewController } from "@/app/controller/useViewController";
 
+import { observer } from "mobx-react-lite";
 import styles from "./SearchBar.module.css";
 
-export const SearchBar = () => {
+export const SearchBar = observer(() => {
   const viewController = useViewController();
-  const [inputText, setInputText] = useState(viewController.searchQuery);
   const [searchFocused, setSearchFocused] = useState(false);
-
-  const handleTextChange = useCallback(
-    (newText: string) => {
-      // We have both this local state and the state in the ViewController in order to
-      // make the input properly responsive. In the onChange event we update both.
-      setInputText(newText);
-      viewController.setSearchQuery(newText);
-    },
-    [viewController],
-  );
-
   return (
     <div
       className={searchFocused ? styles.SearchFocus : styles.Search}
@@ -31,8 +20,8 @@ export const SearchBar = () => {
         type="search"
         placeholder="Search..."
         className={styles.SearchContent}
-        value={inputText}
-        onChange={(e) => handleTextChange(e.target.value)}
+        value={viewController.searchQuery}
+        onChange={(e) => viewController.setSearchQuery(e.target.value)}
       />
       {viewController.searchQuery && (
         <button onClick={() => viewController.setSearchQuery("")}>
@@ -41,4 +30,4 @@ export const SearchBar = () => {
       )}
     </div>
   );
-};
+});
