7ce53e305fa4c28ee3bd2de0a6d3ab7e6900da93
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-05-15 12:02:21 -0400

    Only show relation count box if it's greater than zero

--- src/app/components/RelatedObject/RelatedObjectView.tsx
+++ src/app/components/RelatedObject/RelatedObjectView.tsx
@@ -218,7 +218,9 @@ export const RelatedObjectView = observer(
                 </div>
                 {viewController.showNodeDetails && viewType !== "replace" && <RelatedObjectDetails />}
               </div>
-              <div className="relative -top-[2px] border text-gray-400 px-1">{children.length}</div>
+              {children.length > 0 && (
+                <div className="relative -top-[2px] border text-gray-400 px-1">{children.length}</div>
+              )}
             </div>
           </RelationAtPathProvider>
           {showChildren && (
