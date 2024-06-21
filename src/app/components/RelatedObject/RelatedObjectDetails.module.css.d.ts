584cb0649d05e145cb9b93bb5569b35f9a55a342
Agu <agugalgui@gmail.com>
2024-06-21 14:13:34 +0200

    RelatedObjectDetail CSS refactor (#220)

---
+++ src/app/components/RelatedObject/RelatedObjectDetails.module.css.d.ts
@@ -0,0 +1,5 @@
+declare const styles: {
+  readonly "DetailsContainer": string;
+};
+export = styles;
+
--- src/app/components/RelatedObject/RelatedObjectDetails.tsx
+++ src/app/components/RelatedObject/RelatedObjectDetails.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react-lite";
 
+import styles from "@/app/components/RelatedObject/RelatedObjectDetails.module.css";
 import { GraphNode } from "@/app/graph/GraphNode";
 import { GraphObject } from "@/app/graph/GraphObject";
 import { GraphRelation } from "@/app/graph/GraphRelation";
@@ -32,21 +33,19 @@ export const RelatedObjectDetails = observer(
     const parentZones = getParentZones(relation);
 
     return (
-      <div style={{ display: "flex", fontSize: "0.75rem", gap: "10px" }}>
+      <div className={styles.DetailsContainer}>
         {position && (
-          <span style={{ color: "gray" }}>
+          <span>
             position: {position.int}-{position.frac}
           </span>
         )}
-        <span style={{ color: "gray" }}>id: {object.id}</span>
-        <span style={{ color: "gray" }}>relationId: {relation.id}</span>
-        <span style={{ color: "gray" }}>createdAt: {object.createdAt.toISOString()}</span>
-        {object instanceof GraphNode && object.isBundle && <span style={{ color: "gray" }}>#BUNDLE</span>}
-        {object instanceof GraphNode && object.isZone && <span style={{ color: "gray" }}>#ZONE</span>}
-        {bundles && <span style={{ color: "gray" }}>part of bundle: {bundles.map((b) => b.id).join(", ")}</span>}
-        {parentZones.length > 0 && (
-          <span style={{ color: "gray" }}>zones: {parentZones.map((z) => `${z.id}:"${z.text}"`).join(", ")}</span>
-        )}
+        <span>id: {object.id}</span>
+        <span>relationId: {relation.id}</span>
+        <span>createdAt: {object.createdAt.toISOString()}</span>
+        {object instanceof GraphNode && object.isBundle && <span>#BUNDLE</span>}
+        {object instanceof GraphNode && object.isZone && <span>#ZONE</span>}
+        {bundles && <span>part of bundle: {bundles.map((b) => b.id).join(", ")}</span>}
+        {parentZones.length > 0 && <span>zones: {parentZones.map((z) => `${z.id}:"${z.text}"`).join(", ")}</span>}
       </div>
     );
   },
