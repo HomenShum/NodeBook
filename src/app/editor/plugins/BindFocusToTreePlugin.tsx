a1a1194d762ba3d2dd131e36681777318857431b
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-08-30 12:19:02 -0400

    Fix: Moving a bullet with cmd+shift+down freezes the app (#377)
    The steps described in the ticket get you into an infinite loop of
    focusing and blurring. I started trying to pin-point it down, but it was
    a bit of a slog, so I just read through the code and did what made sense
    instead, which seems to have fixed it.
    
    The gist is that I added some if conditions to the handlers of
    BindFocusToTreePlugin which check the current state of the tree
    selection and editor before deciding if we should change the tree/editor
    focus:
    
    - On selection change, we check if there's mismatch b/w tree selection
    and editor and if there is, we focus/blur the editor accordingly
    - When the editor receives a blur/focus command, we check if the tree
    selection and editor both say the current node is/isn't focused, and if
    they agree, we update the tree selection according to the blur/focus
    event.
    
    Completes ENT-3920
    
    (While reviewing #370 I noticed this PR fixes ENT-3951 too)
    
    ## How to test
    
    See replication steps on ticket. On this branch, they don't cause
    freezing anymore.
    https://linear.app/ideaflow/issue/ENT-3920/moving-a-bullet-with-cmdshiftdown-freezes-the-app

--- src/app/editor/plugins/BindFocusToTreePlugin.tsx
+++ src/app/editor/plugins/BindFocusToTreePlugin.tsx
@@ -15,28 +15,30 @@ export const BindFocusToTreePlugin = observer(() => {
   const { treeNode } = useTreeNode();
 
   useEffect(() => {
+    function isEditorFocused() {
+      return editor.getRootElement()?.contains(document.activeElement);
+    }
+
     // Update the editor focus to match the tree selection
     const disposeAutorun = autorun(() => {
-      const editorFocused = editor.getRootElement()?.contains(document.activeElement);
       const sel = tree.selection;
 
       switch (sel?.type) {
         case "node": {
-          if(editorFocused){
+          if (isEditorFocused()) {
+            // When the selection switches to node type, blur all editors
             editor.blur();
           }
           break;
         }
         case "editor": {
-          if(tree.isNodeFocused(treeNode.id)){
-            editor.update(() => {
-              editor.focus(undefined, {
-                defaultSelection: sel.position === "start" ?
-                  "rootStart" : sel.position === "end"
-                    ? "rootEnd" : undefined
-              });
-            })
-          } else {
+          if (!isEditorFocused() && sel.treeNodeId === treeNode.id) {
+            // Editor isn't focused but should be -> focus it
+            editor.focus(undefined, {
+              defaultSelection: sel.position === "start" ? "rootStart" : sel.position === "end" ? "rootEnd" : undefined,
+            });
+          } else if (isEditorFocused() && sel.treeNodeId !== treeNode.id) {
+            // Editor is focused but shouldn't be -> blur it
             editor.blur();
           }
         }
@@ -48,7 +50,8 @@ export const BindFocusToTreePlugin = observer(() => {
       editor.registerCommand(
         BLUR_COMMAND,
         action(() => {
-          if (tree.isNodeFocused(treeNode.id)) {
+          if (isEditorFocused() && tree.isNodeFocused(treeNode.id)) {
+            // Editor is blurring while node is focused -> set tree selection to null
             tree.setFocusedNode(null);
           }
           return false;
@@ -58,20 +61,25 @@ export const BindFocusToTreePlugin = observer(() => {
       editor.registerCommand(
         FOCUS_COMMAND,
         action(() => {
-          //`setFocusedNode` has a optional position param that defaults to "end",
-          // now because of that the editor and the Tree get in an inconsistent state,
-          // If we call setFocusedNode(newNodePath, "start") from anywhere, it triggers
-          // the editor focus (in the autorun above) but then that focus dispatches
-          // this FOCUS_COMMAND without a position, which defaults to end, but since we set
-          // selection position to `true` initially, it triggers the autorun again).
-          // Hence we "compute" the position from the editorState and pass it to
-          // `setFocusedNode` here.
-          let position: EditorSelectionPosition = "end";
-          const startEndPoints = editor.getEditorState()._selection?.getStartEndPoints();
-          if(Array.isArray(startEndPoints) && startEndPoints[0].offset === 0 && startEndPoints[1].offset === 0){
-            position = "start";
+          if (!isEditorFocused() && !tree.isNodeFocused(treeNode.id)) {
+            // Editor is becoming focused but node isn't focused -> set tree selection to this node
+
+            //`setFocusedNode` has a optional position param that defaults to "end",
+            // now because of that the editor and the Tree get in an inconsistent state,
+            // If we call setFocusedNode(newNodePath, "start") from anywhere, it triggers
+            // the editor focus (in the autorun above) but then that focus dispatches
+            // this FOCUS_COMMAND without a position, which defaults to end, but since we set
+            // selection position to `true` initially, it triggers the autorun again).
+            // Hence we "compute" the position from the editorState and pass it to
+            // `setFocusedNode` here.
+            let position: EditorSelectionPosition = "end";
+            const startEndPoints = editor.getEditorState()._selection?.getStartEndPoints();
+            if (Array.isArray(startEndPoints) && startEndPoints[0].offset === 0 && startEndPoints[1].offset === 0) {
+              position = "start";
+            }
+            tree.setFocusedNode(treeNode.id, position);
+            return true;
           }
-          tree.setFocusedNode(treeNode.id, position);
           return false;
         }),
         COMMAND_PRIORITY_EDITOR,
