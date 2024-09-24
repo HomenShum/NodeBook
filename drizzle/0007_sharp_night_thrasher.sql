20a3f3ea47025f0d125ca464500fd2400aa2925e
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-24 10:00:02 -0400

    fix migration script

--- drizzle/0007_sharp_night_thrasher.sql
+++ drizzle/0007_sharp_night_thrasher.sql
@@ -1,2 +1,2 @@
 -- Custom SQL migration file, put you code below! --
-UPDATE "relation_type" SET "reverseLabel"='sublist of' WHERE id = "sublist";
+UPDATE "relation_type" SET "reverseLabel"='sublist of' WHERE id = 'sublist';
