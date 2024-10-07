1ce51ac4622bf30c717dd5331438414d051436d6
RPC <67982490+rpc333@users.noreply.github.com>
2024-10-07 12:15:30 -0600

    Hot fix for graph loading (#514)
    Pretty sure we're getting crashes in prod from the
    `isNewRelatedObjectsPublic` code trying to update a non-node here
    
    https://ideaflow.sentry.io/issues/5964105966/?project=4507929695617024&query=error.unhandled%3Atrue%20is%3Aunresolved&referrer=issue-stream&sort=date&statsPeriod=14d&stream_index=0
    
    Immediate easy fix is to just insist that the `to` is an instance of
    `GraphNode`. This isn't quite what we want as overall product behavior,
    but we need to come back to this feature for full `GraphObject`
    polymorphism later anyway so I'm ok with this for right now.

--- src/app/graph/GraphStore.ts
+++ src/app/graph/GraphStore.ts
@@ -1063,6 +1063,7 @@ export class GraphStore {
 
       if (
         !relationProps.to.isPublic &&
+        relationProps.to instanceof GraphNode &&
         relationProps.from.isPublic &&
         relationProps.from instanceof GraphNode &&
         relationProps.from.isNewRelatedObjectsPublic
@@ -1803,20 +1804,14 @@ export class GraphStore {
     const relationsById = serializeMap(this.relationsById);
     const relationTypesById = toJS(this.relationTypesById);
 
-    const relationsByNodeId = Array.from(this.nodesById.values()).reduce(
-      (acc, node) => {
-        acc[node.id] = node.allRelationsList.serialize();
-        return acc;
-      },
-      {} as Record<string, SerializedPositionList<GraphRelation>>,
-    );
-    const pinnedRelationsByNodeId = Array.from(this.nodesById.values()).reduce(
-      (acc, node) => {
-        acc[node.id] = node.pinnedRelationsList.serialize();
-        return acc;
-      },
-      {} as Record<string, SerializedPositionList<GraphRelation>>,
-    );
+    const relationsByNodeId = Array.from(this.nodesById.values()).reduce((acc, node) => {
+      acc[node.id] = node.allRelationsList.serialize();
+      return acc;
+    }, {} as Record<string, SerializedPositionList<GraphRelation>>);
+    const pinnedRelationsByNodeId = Array.from(this.nodesById.values()).reduce((acc, node) => {
+      acc[node.id] = node.pinnedRelationsList.serialize();
+      return acc;
+    }, {} as Record<string, SerializedPositionList<GraphRelation>>);
 
     const relationToBundles = serializeMapWithArrayValues(this.relationToBundles);
 
