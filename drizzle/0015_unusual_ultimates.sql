15f94b01d179b5495dfb4b92b2b19893f263f14b
Piyush Jha <piyushjha8164@gmail.com>
2024-10-16 10:26:48 +0530

    use `gen_random_uuid` instead of hardcoded uuids

--- drizzle/0015_unusual_ultimates.sql
+++ drizzle/0015_unusual_ultimates.sql
@@ -2,7 +2,7 @@ UPDATE relation_lists SET is_public = true FROM graph_relation WHERE relation_li
 
 INSERT INTO relation_lists
 (id, node_id, relation_id, pinned, position_int , position_frac, author_id, is_public)
-SELECT 'e55c0e1a-6a4a-43b5-83a2-6d3173f25d17', 'global-users-id', 'global-root-to-users', false, 1728804849594, 'a0', 'global-admin-user-id', true
+SELECT gen_random_uuid(), 'global-users-id', 'global-root-to-users', false, 1728804849594, 'a0', 'global-admin-user-id', true
 WHERE NOT EXISTS (
     SELECT 1
     FROM relation_lists
@@ -13,7 +13,7 @@ WHERE NOT EXISTS (
 
 INSERT INTO relation_lists
 (id, node_id, relation_id, pinned, position_int , position_frac, author_id, is_public)
-SELECT '02f4e4f6-487e-4b82-b6de-dfef7c2223f9', 'global-root-id', 'global-root-to-users', false, 1725586109289, 'aO', 'global-admin-user-id', true
+SELECT gen_random_uuid(), 'global-root-id', 'global-root-to-users', false, 1725586109289, 'aO', 'global-admin-user-id', true
 WHERE NOT EXISTS (
     SELECT 1
     FROM relation_lists
