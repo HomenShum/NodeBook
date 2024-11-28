b4e9445694f5a4b623ad9af33b9a443a51617b6e
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-28 10:41:28 -0500

    update script for updating demo deploys

--- scripts/update-demos.sh
+++ scripts/update-demos.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -e
+
+# Create a copy of the original .env.local file
+cp .env.local .env.local.copy
+
 echo "Updating main branch"
 git checkout main && git pull origin main
 
-echo "Updating lidemo branch"
-git checkout lidemo && git pull && git merge main --no-edit && git push
+# List of demo branches to update
+demo_branches=("lidemo" "lippdemo" "scrapedemo")
 
-echo "Updating lippdemo branch"
-git checkout lippdemo && git pull && git merge main --no-edit && git push
+for branch in "${demo_branches[@]}"; do
+  echo "Updating ${branch} branch"
+  git checkout "$branch"
+  git pull
+  git merge main --no-edit
+  git push
+  
+  echo "Migrating ${branch} branch"
+  yarn vercel env pull --environment=preview --git-branch="$branch" .env.local
+  yarn db:migrate
+done
 
-echo "Updating scrapedemo branch"
-git checkout scrapedemo && git pull && git merge main --no-edit && git push
-
+# Reset .env.local to the original state
 git checkout main
+cp .env.local.copy .env.local
+rm .env.local.copy
+
 echo "Done"
