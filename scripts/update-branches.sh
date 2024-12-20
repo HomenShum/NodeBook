f867b8aae36e335a72cca3af1ec44de8cc0178db
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-12-20 12:11:29 -0500

    Update the update-branches script and workflow to not require checking out and pulling each branch (#755)

--- scripts/update-branches.sh
+++ scripts/update-branches.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+# This script updates all branches specified below to match the main.
+# Each of these branches has a corresponding preview deployment on Vercel which we
+# want to keep in sync with the main branch.
+#
+# Usage:
+#
+# When running locally with an already linked vercel project, you can run the script
+# without the vercel token.
+# >>> ./update-branches.sh
+#
+# If running in CI, you need to pass the vercel token as an argument.
+# >>> ./update-branches.sh <vercel-token>
 set -e
 
+branches=("mew-lite" "lidemo" "lidemo-lite" "lippdemo" "lippdemo-lite" "scrapedemo" "scrapedemo-lite")
 vercel_token=$1
 
 # Create a copy of the original .env.local file
@@ -9,21 +22,16 @@ if [ -f .env.local ]; then
 fi
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-echo "Updating main branch"
-git checkout main && git pull origin main
-
-branches=("mew-lite" "lidemo" "lidemo-lite" "lippdemo" "lippdemo-lite" "scrapedemo" "scrapedemo-lite")
-
+# Link the vercel project if we have a token
 if [ -n "$vercel_token" ]; then
   yarn vercel link --yes --project prj_M9QeEZasw5AkcJ8Z6sf2WCwelhjz --token="$vercel_token"
 fi
 
+git fetch origin main
 for branch in "${branches[@]}"; do
-  echo "Updating ${branch} branch"
-  git checkout "$branch"
-  git pull
-  git merge main --no-edit
-  git push
+  echo "Updating ${branch}"
+  # FETCH_HEAD refers to main since we just fetched it above
+  git push origin FETCH_HEAD:${branch} --force
   
   echo "Migrating ${branch} branch"
   if [ -n "$vercel_token" ]; then
@@ -35,7 +43,6 @@ for branch in "${branches[@]}"; do
 done
 
 # Reset .env.local to the original state
-git checkout main
 if [ -f .env.local.copy ]; then
   # If we had an original .env.local, restore it from the backup
   mv .env.local.copy .env.local
