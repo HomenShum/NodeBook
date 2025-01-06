44e8857e623876210f2bc591c2a8864eb49e9a9f
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-06 12:10:53 -0500

    Revert "ENT-4566: Arrow key navigation skips lines instead of moving to end" (#782)
    Reverts IdeaFlowCo/mew#770
    
    The PR introduced a bug where typing " x" and then pressing backspace
    would delete the "x" AND the " ". Also after that, pressing space stops
    working. Here's a video showing the behaviour and that removing
    `WordBreakPlugin` fixes it:
    https://www.loom.com/share/09e6d25243f1418da98a96074b4ca4df

--- src/app/editor/NodeContentEditor.tsx
+++ src/app/editor/NodeContentEditor.tsx
@@ -27,7 +27,6 @@ import { useClickableMention } from "@/app/editor/utils/useClickableMention";
 import { GraphNode } from "@/app/graph/GraphNode";
 import { MentionNode } from "@/app/graph/MentionNode";
 import { DescendantTreeNode } from "@/app/tree/nodes";
-import { WordBreakPlugin } from "@/app/editor/plugins/WordBreakPlugin";
 
 import styles from "./Editor.module.css";
 
@@ -62,7 +61,6 @@ export const NodeEditor = observer(function NodeEditor({ treeNode, isEditorEdita
           placeholder={<span className={styles.PlaceholderNode}>Start writing...</span>}
         />
         <SyncWithModelsPlugin node={treeNode.object} treeNode={treeNode} />
-        {isEditorEditable && <WordBreakPlugin />}
         {isEditorEditable && <LinkPlugin />}
         {isEditorEditable && <ReplacementPlugin treeNode={treeNode} />}
         {isEditorEditable && <ClearEditorPlugin />}
