a0802d40994476d128c21fa428a3643f44d6f175
Samuel Xu <samuelzxu@gmail.com>
2025-03-03 12:02:40 -0500

    Create new node button (#966)
    ## Summary
    
    Create a new node button changes
    
    ## How to test
    
    Create new node button update
    
    ## Checklist
    
    - [X] I followed the [style
    guide](https://www.notion.so/Style-guide-08041425021b49b28a0734f4e8105c33).
    - [X] I tested my contribution.
    - [ ] I added new environment variables to
    [Vercel](https://vercel.com/ideaflowco/mew/settings/environment-variables).

--- src/app/components/Buttons/CreateNewButton.module.css.d.ts
+++ src/app/components/Buttons/CreateNewButton.module.css.d.ts
@@ -1,4 +1,5 @@
 declare const styles: {
+  readonly "FullWidthWrapper": string;
   readonly "ShowTooltip": string;
   readonly "ToLeftAlign": string;
 };
--- src/app/components/Buttons/CreateNewButton.tsx
+++ src/app/components/Buttons/CreateNewButton.tsx
@@ -7,18 +7,22 @@ import { cn } from "@/lib/utils";
 import styles from "./CreateNewButton.module.css";
 
 export function CreateNewButton({ tree }: { tree: Tree }) {
+  const handleClick = async (e: React.MouseEvent) => {
+    e.stopPropagation();
+    await tree.createChildOfRootAndFocus();
+  };
+
   return (
-    <Button
-      className={cn(styles.ShowTooltip, styles.ToLeftAlign)}
-      data-tooltip="Add node"
-      variant="ghostSmooth"
-      size="xs"
-      onClick={async (e) => {
-        e.stopPropagation();
-        await tree.createChildOfRootAndFocus();
-      }}
-    >
-      <Plus size={14}></Plus>
-    </Button>
+    <div className={styles.FullWidthWrapper} onClick={handleClick}>
+      <Button
+        className={cn(styles.ShowTooltip, styles.ToLeftAlign)}
+        data-tooltip="Add node"
+        variant="ghostSmooth"
+        size="xs"
+        onClick={handleClick}
+      >
+        <Plus size={14}></Plus>
+      </Button>
+    </div>
   );
 }
