a4bcc2a405e49d2ab42c4057ae2663757fc87141
Samuel Xu <samuelzxu@gmail.com>
2025-03-06 10:18:11 -0500

    Root issues on first load (#986)
    ## Summary
    
    Skips root setting when the app is loading.
    
    ## How to test
    
    Load the app on your local setup
    
    ## Checklist
    
    - [X] I followed the [style
    guide](https://www.notion.so/Style-guide-08041425021b49b28a0734f4e8105c33).
    - [X] I tested my contribution.
    - [ ] I added new environment variables to
    [Vercel](https://vercel.com/ideaflowco/mew/settings/environment-variables).

--- src/app/g/[[...path]]/page.tsx
+++ src/app/g/[[...path]]/page.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 
 import { MainView } from "@/app/components/MainView";
 import { useGraphStore } from "@/app/contexts/GraphStoreContext";
+import { useLoading } from "@/app/contexts/LoadingContext";
 import { useSetMainRoot } from "@/app/tree/utils";
 import { parsePathArray } from "@/app/util";
 import { useViewStore } from "@/app/view/useViewStore";
@@ -13,8 +14,14 @@ function Page({ params: { path: pathArray } }: { params: { path: string[] | unde
   const viewStore = useViewStore();
   const graphStore = useGraphStore();
   const setRoot = useSetMainRoot();
+  const isLoading = useLoading();
 
   useEffect(() => {
+    if (isLoading) {
+      logger.debug("App is still loading, skipping root setting");
+      return;
+    }
+
     const path = parsePathArray(pathArray ?? [], graphStore);
     // If the path is valid, keep the path, and update our view state to match
     // Otherwise, we redirect to the specified object or the default root
@@ -42,7 +49,7 @@ function Page({ params: { path: pathArray } }: { params: { path: string[] | unde
     // To match that, we only run this effect when viewStore or graphStore are instantiated
     // and specifically exclude path as a dependency here.
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [viewStore, graphStore, setRoot]);
+  }, [viewStore, graphStore, setRoot, isLoading]);
 
   return <MainView tree={viewStore.mainView}></MainView>;
 }
