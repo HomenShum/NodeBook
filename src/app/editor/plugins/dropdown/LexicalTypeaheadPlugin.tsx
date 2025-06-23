7a407897c00af042cd9deb5bf8fd1bd1e5b120c1
Piyush Jha <piyushjha8164@gmail.com>
2025-06-23 21:54:11 +0530

    chore: housekeeping, delete redundant code

--- src/app/editor/plugins/dropdown/LexicalTypeaheadPlugin.tsx
+++ src/app/editor/plugins/dropdown/LexicalTypeaheadPlugin.tsx
@@ -100,59 +100,6 @@ function isSelectionOnEntityBoundary(editor: LexicalEditor, offset: number): boo
   });
 }
 
-// Got from https://stackoverflow.com/a/42543908/2013580
-export function getScrollParent(element: HTMLElement, includeHidden: boolean): HTMLElement | HTMLBodyElement {
-  let style = getComputedStyle(element);
-  const excludeStaticParent = style.position === "absolute";
-  const overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
-  if (style.position === "fixed") {
-    return document.body;
-  }
-  for (let parent: HTMLElement | null = element; (parent = parent.parentElement); ) {
-    style = getComputedStyle(parent);
-    if (excludeStaticParent && style.position === "static") {
-      continue;
-    }
-    if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX)) {
-      return parent;
-    }
-  }
-  return document.body;
-}
-
-export const SCROLL_TYPEAHEAD_OPTION_INTO_VIEW_COMMAND: LexicalCommand<{
-  index: number;
-  option: MenuOption;
-}> = createCommand("SCROLL_TYPEAHEAD_OPTION_INTO_VIEW_COMMAND");
-
-export function useBasicTypeaheadTriggerMatch(
-  trigger: string,
-  { minLength = 1, maxLength = 75 }: { minLength?: number; maxLength?: number },
-): TriggerFn {
-  return useCallback(
-    (text: string) => {
-      const validChars = "[^" + trigger + PUNCTUATION + "\\s]";
-      const TypeaheadTriggerRegex = new RegExp(
-        "(^|\\s|\\()(" + "[" + trigger + "]" + "((?:" + validChars + "){0," + maxLength + "})" + ")$",
-      );
-      const match = TypeaheadTriggerRegex.exec(text);
-      if (match !== null) {
-        const maybeLeadingWhitespace = match[1];
-        const matchingString = match[3];
-        if (matchingString.length >= minLength) {
-          return {
-            leadOffset: match.index + maybeLeadingWhitespace.length,
-            matchingString,
-            replaceableString: match[2],
-          };
-        }
-      }
-      return null;
-    },
-    [maxLength, minLength, trigger],
-  );
-}
-
 export type TypeaheadMenuPluginProps<TOption extends MenuOption> = {
   onQueryChange: (matchingString: string | null) => void;
   onSelectOption: (
